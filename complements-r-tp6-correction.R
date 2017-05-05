5b83893aaa0634fe3a43028be7067998bd8bfd31
fxjollois <fxjollois@gmail.com>
2017-05-05 12:18:00 +0200

    maj TP6 comp R

--- complements-r-tp6-correction.R
+++ complements-r-tp6-correction.R
@@ -24,30 +24,41 @@ runApp(shinyApp(
             tabItems(
                 tabItem(
                     "resume",
-                    box(
+                    tabBox(
                         title = "Volume des ventes",
-                        plotOutput("conso"),
-                        footer = "Données provenant de txhousing",
-                        status = "info",
-                        solidHeader = TRUE,
+                        tabPanel("Courbe", plotOutput("conso")),
+                        tabPanel("Tableau", tableOutput("consotab")),
                         width = 8
                     ),
                     uiOutput("progression"),
-                    valueBox(
-                        value = "?",
-                        subtitle = "Volume totale des ventes",
-                        icon = icon("usd"),
-                        color = "light-blue"
-                    ),
+                    uiOutput("volume"),
                     tabBox(
                         title = "Informations",
                         width = 4,
-                        tabPanel("Nombre", "Nombre de ventes"),
-                        tabPanel("Prix médian", "prix médian")
+                        tabPanel("Nombre", "Nombre de ventes", uiOutput("nombre")),
+                        tabPanel("Prix médian", "Prix médian", uiOutput("prixmed"))
                     )
                 ),
                 tabItem(
-                    "top"
+                    "top",
+                    box(
+                        "Villes",
+                        width = 4,
+                        tableOutput("topvilles"),
+                        radioButtons("nbvilles", "Taille du TOP", c(3, 5, 10), inline = T)
+                    ),
+                    box(
+                        "Années",
+                        width = 4,
+                        tableOutput("topannees"),
+                        radioButtons("nbannees", "Taille du TOP", c(3, 5, 10), inline = T)
+                    ),
+                    box(
+                        "Mois",
+                        width = 4,
+                        tableOutput("topmois"),
+                        radioButtons("nbmois", "Taille du TOP", c(3, 5, 10), inline = T)
+                    )
                 )
             )
         ),
@@ -80,7 +91,8 @@ runApp(shinyApp(
             
             
             valeur = round(med2015 / med2000 * 100, 2)
-            if (is.na(valeur)) {
+            # Ajout du test pour savoir si la progression a pu être calculée
+            if (is.na(valeur)) { # valeur == NA -> Problème sur 2000 a priori
                 infoBox(
                     title = "Progression",
                     value = "Erreur",
@@ -88,7 +100,7 @@ runApp(shinyApp(
                     icon = icon("warning"),
                     color = "yellow"
                 )
-            } else {
+            } else { # valeur OK -> on l'affiche
                 couleur = ifelse(valeur < 200, "red", ifelse(valeur < 350, "light-blue", "green"))
                 infoBox(
                     title = "Progression",
@@ -101,5 +113,82 @@ runApp(shinyApp(
             }
             
         })
+        
+        # valueBox() en fonction de la ville choisie
+        output$volume <- renderUI({
+            if (input$ville == "Toutes les villes") {
+                df = txhousing
+            } else {
+                df = subset(txhousing, city == input$ville)
+            }
+            valeur = sum(df$volume, na.rm = TRUE)
+            couleur = ifelse(valeur < 2e9, "red", ifelse(valeur < 50e9, "light-blue", "green"))
+            valueBox(
+                value = round(valeur / 1e9, 2),
+                subtitle = "Volume totale des ventes (en milliards)",
+                icon = icon("usd"),
+                color = couleur
+            )
+        })
+        
+        # nombre total de ventes
+        output$nombre <- renderUI({
+            if (input$ville == "Toutes les villes") {
+                df = txhousing
+            } else {
+                df = subset(txhousing, city == input$ville)
+            }
+            valeur = sum(df$sales, na.rm = TRUE)
+            p(valeur)
+        })
+        
+        # prix médian de ventes
+        output$prixmed <- renderUI({
+            if (input$ville == "Toutes les villes") {
+                df = txhousing
+            } else {
+                df = subset(txhousing, city == input$ville)
+            }
+            valeur = median(df$median, na.rm = TRUE)
+            p(valeur)
+        })
+        
+        # tableau des consommation, année par année et mois par mois
+        output$consotab <- renderTable({
+            if (input$ville == "Toutes les villes") {
+                df = txhousing
+            } else {
+                df = subset(txhousing, city == input$ville)
+            }
+            a = aggregate(volume ~ year + month, df, sum)
+            b = acast(a, year ~ month, value.var = "volume")
+            c = b / 1e6
+        }, rownames = TRUE, digits = 0, na = "-")
+        
+        # TOP Villes
+        output$topvilles <- renderTable({
+          df = aggregate(volume / 1e9 ~ city, txhousing, sum)  
+          df = df[order(df$volume, decreasing = T),]
+          nb = as.numeric(input$nbvilles)
+          names(df) = c("Ville", "Volume")
+          head(df, nb)
+        }, digits = 2)
+        # TOP Année
+        output$topannees <- renderTable({
+            df = aggregate(volume / 1e9 ~ year, txhousing, sum)  
+            df = df[order(df$volume, decreasing = T),]
+            nb = as.numeric(input$nbannees)
+            names(df) = c("Année", "Volume")
+            head(df, nb)
+        }, digits = 2)
+        # TOP Mois
+        output$topmois <- renderTable({
+            df = aggregate(volume / 1e9 ~ month, txhousing, sum)  
+            df = df[order(df$volume, decreasing = T),]
+            nb = as.numeric(input$nbmois)
+            names(df) = c("Mois", "Volume")
+            head(df, nb)
+        }, digits = 2)
+        
     }
 ))
