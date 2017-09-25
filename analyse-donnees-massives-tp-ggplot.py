277f2e1c7d6a0f0d009a8c28783a095e14586418
fxjollois <fxjolloisupd@gmail.com>
2017-09-25 12:03:52 +0200

    modification index.md -> index.html pour jekyll

--- analyse-donnees-massives-tp-ggplot.py
+++
@@ -1,5 +0,0 @@
-ggplot(aes("Sepal.Length"), data=iris) + geom_histogram()
-ggplot(aes("Sepal.Length"), data=iris) + geom_density()
-ggplot(aes("Sepal.Length"), data=iris) +\
-    geom_histogram(y = "..density..") +\
-    geom_density()
